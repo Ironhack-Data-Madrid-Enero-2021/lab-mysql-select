9d36ec92a7c61b62d70134eeda2b7e216f7de9ca
Sonia Fominaya <fominayasg@gmail.com>
2021-01-27 22:14:15 +0100

    challenge 1 y 2

---
+++ your-code/solutions.sql
@@ -0,0 +1,22 @@
+## Challenge 1 - Who Have Published What At Where?
+
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME',a.au_fname AS 'FIRST NAME', t.title AS 'TITLE', pub.pub_name AS 'PUBLISHER'
+FROM titleauthor as ta
+	JOIN authors as a
+		on a.au_id = ta.au_id
+	JOIN titles as t
+		on ta.title_id = t.title_id
+	JOIN publishers as pub
+		on pub.pub_id = t.pub_id;
+        
+## Challenge 2 - Who Have Published How Many At Where?
+
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME',a.au_fname AS 'FIRST NAME', pub.pub_name AS 'PUBLISHER',  COUNT(t.title) AS 'TITLE_COUNT'
+FROM titleauthor as ta
+	JOIN authors as a
+		on a.au_id = ta.au_id
+	JOIN titles as t
+		on ta.title_id = t.title_id
+	JOIN publishers as pub
+		on pub.pub_id = t.pub_id
+GROUP BY  a.au_id, a.au_lname, a.au_fname, pub.pub_name;
