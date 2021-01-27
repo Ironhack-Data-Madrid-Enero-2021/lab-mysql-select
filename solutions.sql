46a90b43c1d69d20eae9f130c1fbf338d7769236
XabierArrieta <xabierarrietachango@gmail.com>
2021-01-28 00:16:57 +0100

    ysql

--- solutions.sql
+++ solutions.sql
@@ -1,4 +1,5 @@
 -- Challenge1
+
 USE Ironhack;
 SELECT
 authors.au_id AS "AUTHOR ID",
@@ -13,7 +14,9 @@ JOIN titles
     ON  titleauthor.title_id = titles.title_id
 JOIN publishers
     ON  titles.pub_id = publishers.pub_id;
+    
 -- Challenge2
+
 USE Ironhack;
 SELECT 
 authors.au_id AS 'AUTHOR ID',
@@ -33,4 +36,48 @@ JOIN publishers
 	ON	titles.pub_id = publishers.pub_id
 
 GROUP BY authors.au_id, pub_name;
+
 -- Challenge3
+
+USE Ironhack;
+SELECT 
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME', 
+authors.au_fname AS 'FIRST NAME',
+
+SUM(sales.qty * titles.price) AS 'TOTAL'
+
+FROM authors
+
+JOIN titleauthor
+	ON	authors.au_id = titleauthor.au_id
+JOIN titles
+	ON	titleauthor.title_id = titles.title_id
+JOIN sales
+	ON	titles.title_id = sales.title_id
+
+GROUP BY authors.au_id
+ORDER BY 'TOTAL' DESC
+LIMIT 3
+
+-- Challenge 4
+
+USE Ironhack;
+SELECT 
+authors.au_id AS 'AUTHOR ID',
+authors.au_lname AS 'LAST NAME', 
+authors.au_fname AS 'FIRST NAME',
+
+COALESCE (SUM(sales.qty * titles.price),0) AS 'TOTAL'
+
+FROM authors
+
+JOIN titleauthor
+	ON	authors.au_id = titleauthor.au_id
+JOIN titles
+	ON	titleauthor.title_id = titles.title_id
+JOIN sales
+	ON	titles.title_id = sales.title_id
+
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC
