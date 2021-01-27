060a8448251536e76e996770380fa410d5918964
Marina Diez <m.diezs@hotmail.com>
2021-01-27 19:10:38 +0100

    con 4

--- solutions.sql
+++ solutions.sql
@@ -40,5 +40,18 @@ JOIN titleauthor ON authors.au_id = titleauthor.au_id
 JOIN titles ON titles.title_id = titleauthor.title_id
 JOIN sales on titles.title_id = sales.title_id
 GROUP BY authors.au_id
-ORDER BY TOTAL ASC
+ORDER BY TOTAL DESC
 LIMIT 3;
+
+#CHALLENGE 4
+SELECT 
+authors.au_id AS AUTHOR_ID,
+authors.au_lname AS LAST_NAME, 
+authors.au_fname AS FIRST_NAME,
+COALESCE(SUM(sales.qty*titles.price),0) AS TOTAL
+FROM authors
+JOIN titleauthor ON authors.au_id = titleauthor.au_id
+JOIN titles ON titles.title_id = titleauthor.title_id
+JOIN sales on titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY TOTAL DESC;
