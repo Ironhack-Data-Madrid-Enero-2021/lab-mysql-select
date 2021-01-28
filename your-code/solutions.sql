b652a4123970149a88511ec7917969a953773ef2
CristinaCallejo <cristinapazcallejo@gmail.com>
2021-01-28 11:45:36 +0100

    challenge 1 and 2 done, saved resulting csv files

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,34 +1,19 @@
-SELECT authors.au_id, au_lname, au_fname, pub_name FROM authors
-	JOIN titleauthor ON titleauthor.au_id = authors.au_id
-    JOIN titles ON titles.title_id = titleauthor.title_id
-    JOIN publishers ON publishers.pub_id = titles.pub_id
-    
-SELECT authors.pub_name, authors.au_id, COUNT(*) FROM authors
-GROUP BY authors.pub_name ORDER BY authors.pub_name desc;
-
-
-
-
-CHALLENGE 
-SELECT authors.au_id, au_lname, au_fname FROM authors
-JOIN titleauthor ON titleauthor.au_id = authors.au_id
-
-SELECT authors.au_id, au_lname, au_fname, title_id FROM authors
-	JOIN titleauthor ON titleauthor.au_id = authors.au_id
-
-SELECT authors.au_id, au_lname, au_fname, title FROM authors
-	JOIN titleauthor ON titleauthor.au_id = authors.au_id
-    JOIN titles ON titles.title_id = titleauthor.title_id;
-
 SELECT authors.au_id, au_lname, au_fname, title, pub_name FROM authors
 	JOIN titleauthor ON titleauthor.au_id = authors.au_id
     JOIN titles ON titles.title_id = titleauthor.title_id
     JOIN publishers ON publishers.pub_id = titles.pub_id;
 
-SELECT authors.au_id, au_lname, au_fname, pub_name FROM authors
+SELECT authors.au_id, au_lname, au_fname, pub_name, COUNT(pub_name) FROM authors
 	JOIN titleauthor ON titleauthor.au_id = authors.au_id
     JOIN titles ON titles.title_id = titleauthor.title_id
     JOIN publishers ON publishers.pub_id = titles.pub_id
+    GROUP BY pub_name, titleauthor.au_id
+	order by authors.au_id desc;
     
-SELECT authors.pub_name, authors.au_id, COUNT(*) FROM authors
-GROUP BY authors.pub_name ORDER BY authors.pub_name desc;
+
+    
+
+
+
+
+
