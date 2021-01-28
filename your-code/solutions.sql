a3ae3b3ccb4ecfa6b2503a43dadda1b9b351eca7
CristinaCallejo <cristinapazcallejo@gmail.com>
2021-01-28 14:10:40 +0100

    testing and trying chall3

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -9,6 +9,11 @@ SELECT authors.au_id, au_lname, au_fname, pub_name, COUNT(pub_name) FROM authors
     JOIN publishers ON publishers.pub_id = titles.pub_id
     GROUP BY pub_name, titleauthor.au_id
 	order by authors.au_id desc;
+ 
+ SELECT authors.au_id, au_lname, au_fname, au_ord, COUNT(qty) FROM authors   
+	JOIN titleauthor ON titleauthor.au_id = authors.au_id
+    JOIN titles ON titles.title_id = titleauthor.title_id
+    JOIN sales ON sales.title_id = titles.titles.id;
     
 
     
