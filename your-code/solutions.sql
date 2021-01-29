d524e77271a0b314c042586b595c73deb7b4fc2b
Sonia Fominaya <fominayasg@gmail.com>
2021-01-29 12:34:39 +0100

    challenge 3

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -23,13 +23,18 @@ GROUP BY  a.au_id, a.au_lname, a.au_fname, pub.pub_name;
 
 ## Challenge 3 - Best Selling Authors
 
-Me he quedado intentando este, estaba probando con esto de momento
-
-SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME',a.au_fname AS 'FIRST NAME', MAX(sl.qty)  AS 'TOTAL'
+SELECT a.au_id AS 'AUTHOR ID', a.au_lname AS 'LAST NAME',a.au_fname AS 'FIRST NAME', SUM(qty) AS 'TOTAL'
 FROM titleauthor as ta
 	JOIN authors as a
 		on a.au_id = ta.au_id
 	JOIN titles as t
 		on ta.title_id = t.title_id
-	JOIN sales as sl
-		on t.title_id = sl.title_id
+	JOIN publishers as pub
+		on pub.pub_id = t.pub_id
+	JOIN sales AS s
+		on s.title_id = t.title_id
+GROUP BY a.au_id
+ORDER BY `TOTAL`DESC 
+
+## Challenge 4 - Best Selling Authors Ranking
+
